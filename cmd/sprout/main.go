aceeb4ee2524ca2b262e457474f2f4d72b85939a
thejezzi <38394992+thejezzi@users.noreply.github.com>
2025-01-06 21:12:35 +0100

    feat: add argument validation

--- cmd/sprout/main.go
+++ cmd/sprout/main.go
@@ -1,34 +1,78 @@
 package main
 
 import (
+	"errors"
 	"flag"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/thejezzi/gosprout/internal"
 )
 
-func main() {
-	moduleName := flag.String(
-		"module",
-		"testproj",
-		"your module name or path like github.com/you/proj",
-	)
+var (
+	errModuleNameCannotBeZero = errors.New("module name cannot be empty")
+	errPathCannotBeZero       = errors.New("path cannot be empty")
+)
 
-	modulePath := flag.String(
-		"path",
-		"testproj",
-		"the path to put all the files",
-	)
+type arguments struct {
+	path     *string
+	name     *string
+	template *string
+}
 
-	template := flag.String(
-		"template",
-		"simple",
-		"specify a template to avoid some boilerplate setup",
-	)
+// flags parses all flags and returns a structure with all possible arguments or
+// and error that indicates to use the ui mode
+func flags() (*arguments, error) {
+	args := &arguments{
+		name: flag.String(
+			"module",
+			"",
+			"your module name or path like github.com/you/proj",
+		),
 
+		path: flag.String(
+			"path",
+			"",
+			"the path to put all the files",
+		),
+
+		template: flag.String(
+			"template",
+			"simple",
+			"specify a template to avoid some boilerplate setup",
+		),
+	}
 	flag.Parse()
 
-	if err := internal.CreateNewModule(*modulePath, *moduleName, *template); err != nil {
-		log.Fatalf("failed: %v", err)
+	return args.validate()
+}
+
+// validate make sure that all arguments are set to create the project
+func (args *arguments) validate() (*arguments, error) {
+	if len(*args.name) == 0 {
+		return nil, errModuleNameCannotBeZero
+	}
+	if len(*args.path) == 0 {
+		return nil, errPathCannotBeZero
+	}
+
+	return args, nil
+}
+
+// exit prints the top level error and exits with status code one to indicate
+// and error has happened
+func exit(err error) {
+	fmt.Printf("could not create new project: %v\n", err)
+	os.Exit(1)
+}
+
+func main() {
+	args, err := flags()
+	if err != nil {
+		exit(err)
+	}
+
+	if err := internal.CreateNewModule(*args.path, *args.name, *args.template); err != nil {
+		exit(err)
 	}
 }
